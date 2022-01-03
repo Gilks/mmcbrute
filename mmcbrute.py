ac930898146c93928eaad3524e32d4055f1883ad
phx <oldjamey@gmail.com>
2022-01-03 14:19:05 -0600

    fixed indentations

--- mmcbrute.py
+++ mmcbrute.py
@@ -169,11 +169,11 @@ class MMCBrute(object):
 						'\033[94m[!] Honey Badger mode not enabled. Halting to prevent further lockouts..\033[0m')
 					answer = str(raw_input('\033[94m[!] Would you like to proceed with the bruteforce? (Y/N) '))
 					if answer.lower() in ["y", "yes", ""]:
-							self.logger.info('\033[93m[*] Resuming...')
-							return False
+						self.logger.info('\033[93m[*] Resuming...')
+						return False
 					else:
-							self.logger.info('\033[91m[-]Exiting...')
-							sys.exit(os.EX_SOFTWARE)
+						self.logger.info('\033[91m[-]Exiting...')
+						sys.exit(os.EX_SOFTWARE)
 
 			elif 'STATUS_PASSWORD_MUST_CHANGE' in msg:
 				self.logger.info(f"\033[92m[+] Success (User never logged in to change password) {attempt}\033[0m")
